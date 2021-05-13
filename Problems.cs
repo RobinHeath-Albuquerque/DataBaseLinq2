01c3f097de66cdeb5ebe134fc984937599747e0e
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-05-13 13:42:44 -0600

    Problem 12

--- Problems.cs
+++ Problems.cs
@@ -26,10 +26,10 @@ namespace DatabaseFirstLINQ
             //ProblemSix();
             //ProblemSeven();
             //ProblemEight();
-            ProblemNine();
+            //ProblemNine();
             //ProblemTen();
-            //ProblemEleven();
-            //ProblemTwelve();
+           // ProblemEleven();
+            ProblemTwelve();
             //ProblemThirteen();
             //ProblemFourteen();
             //ProblemFifteen();
@@ -187,6 +187,14 @@ namespace DatabaseFirstLINQ
         private void ProblemTwelve()
         {
             // Create a new Product object and add that product to the Products table using LINQ.
+            Product newProduct = new Product()
+            {
+                Name = "Coleman Sundome Tent",
+                Description = "Two Person Camping Tent",
+                Price = 49
+            };
+            _context.Products.Add(newProduct);
+            _context.SaveChanges();
 
         }
 
@@ -207,6 +215,8 @@ namespace DatabaseFirstLINQ
         private void ProblemFourteen()
         {
             // Add the product you create to the user we created in the ShoppingCart junction table using LINQ.
+            var productId = _context.Products.Where(p => p.Name == "Coleman Sundome Tent").Select(p => p.Id).SingleOrDefault();
+            
 
         }
 
@@ -224,6 +234,12 @@ namespace DatabaseFirstLINQ
         private void ProblemSixteen()
         {
             // Update the price of the product you created to something different using LINQ.
+            var product = _context.Products.Where(p => p.Name == "Coleman Sundome Tent").SingleOrDefault();
+            product.Price = 69;
+            _context.Products.Update(product);
+            _context.SaveChanges();
+
+            
 
         }
 
